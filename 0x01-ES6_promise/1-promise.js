0e59fa933922aad1bae1525880da313776aac2f3
Johnson adeola <johnsamxy@gmail.com>
2024-03-06 15:59:44 +0100

    new task

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      const obj = { status: 200, body: 'Success' };
+      resolve(obj);
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
