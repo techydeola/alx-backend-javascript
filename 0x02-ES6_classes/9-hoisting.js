5aeed845434951c6d4c7f758cace8d749d32b754
Johnson adeola <johnsamxy@gmail.com>
2024-03-08 00:22:22 +0100

    new task

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,54 @@
+// eslint-disable-next-line guard-for-in, max-classes-per-file
+export class HolbertonClass {
+  constructor(year, location) {
+    // eslint-disable-next-line guard-for-in, no-underscore-dangle
+    this._year = year;
+    // eslint-disable-next-line guard-for-in, no-underscore-dangle
+    this._location = location;
+  }
+
+  get year() {
+    // eslint-disable-next-line guard-for-in, no-underscore-dangle
+    return this._year;
+  }
+
+  get location() {
+    // eslint-disable-next-line guard-for-in, no-underscore-dangle
+    return this._location;
+  }
+}
+
+export class StudentHolberton {
+  constructor(firstName, lastName, holbertonClass) {
+    // eslint-disable-next-line guard-for-in, no-underscore-dangle
+    this._firstName = firstName;
+    // eslint-disable-next-line guard-for-in, no-underscore-dangle
+    this._lastName = lastName;
+    // eslint-disable-next-line guard-for-in, no-underscore-dangle
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    // eslint-disable-next-line guard-for-in, no-underscore-dangle
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this.holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    // eslint-disable-next-line guard-for-in, no-underscore-dangle
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
+
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+export default [student1, student2, student3, student4, student5];
