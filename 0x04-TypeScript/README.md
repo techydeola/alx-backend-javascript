83f8a28a22dceab64d45edc90c8b0ca15650d750
Johnson adeola <johnsamxy@gmail.com>
2024-03-13 16:51:02 +0100

    new task

---
+++ 0x04-TypeScript/README.md
@@ -0,0 +1,4 @@
+### TypeScript
+
+
+- Alx Africa
---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,22 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+};
+
+const student1: Student = {
+  firstName: 'Adeola',
+  lastName: 'Johnson',
+  age: 25,
+  location: 'Nigeria',
+};
+
+const student2: Student = {
+  firstName: 'Julia',
+  lastName: 'Edos',
+  age: 29,
+  location: 'Singapore',
+};
+
+const studentsList = [student1, student2];
