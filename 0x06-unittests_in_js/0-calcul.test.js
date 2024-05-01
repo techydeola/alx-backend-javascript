dbf935a48b346ab5b08b7e0b8c1a64ded250db29
Johnson adeola <johnsamxy@gmail.com>
2024-05-01 19:33:27 +0100

    quick fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,19 @@ const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 
 describe('CalculateNumberTest', function () {
-  it('checks equality', function () {
+  it('should return 4', function () {
     assert.equal(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5', function() {
     assert.equal(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5', function() {
     assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6', function() {
     assert.equal(calculateNumber(1.5, 3.7), 6);
-  })
+  });
 });
