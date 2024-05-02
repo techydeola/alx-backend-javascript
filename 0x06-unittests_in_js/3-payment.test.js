2e9e661a39c6b388cc5b74440c2cc3c1849104b1
Johnson adeola <johnsamxy@gmail.com>
2024-05-02 17:31:59 +0100

    used the assert test

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,14 +1,13 @@
 const sinon = require('sinon');
 const Utils = require('./utils.js');
-const { chai } = require('chai');
-const expect = chai.expect;
+const assert = require('assert');
 const sendPaymentRequestToApi = require('./3-payment.js');
 
 describe('sendPaymentRequestToApi', function() {
   it('shoud correctly log the payment request', function() {
     const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+    assert(spy.calledOnce);
     spy.restore();
   });
 });
